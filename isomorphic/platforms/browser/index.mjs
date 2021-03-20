18d111d835f12a31d080b49b2188f252bf0e1312
HenriqueOmena <88lmao@gmail.com>
2021-03-20 22:12:30 +0000

    Done Browser Side

--- isomorphic/platforms/browser/index.mjs
+++ isomorphic/platforms/browser/index.mjs
@@ -1,5 +1,28 @@
 export default class TableWebComponent {
   render(data) {
-    window.alert("hi table web");
+    const tableTeamplate = this.preparedData(data);
+    document.body.insertAdjacentHTML("afterbegin", tableTeamplate);
+  }
+
+  preparedData(data) {
+    const [first] = data;
+    const joinLists = (list) => list.join("");
+    const tHeaders = Object.keys(first).map((text) => `<th scope=col>${text}</th>`);
+
+    const tBody = data
+      .map((item) => Object.values(item))
+      .map((item) => item.map((value) => `<td>${value}</td>`))
+      .map((tds) => `<tr>${joinLists(tds)}</tr>`);
+
+    return `
+      <table class="table table-dark table-striped">
+        <thead>
+          ${joinLists(tHeaders)}
+        </thead>
+        <tbody>
+          ${joinLists(tBody)}
+        </tbody>
+      </table>
+    `;
   }
 }
