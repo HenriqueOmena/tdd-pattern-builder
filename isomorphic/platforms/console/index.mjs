c21e6228ebdd12b2de4c0f016ccb5392cb4bc6f0
HenriqueOmena <88lmao@gmail.com>
2021-03-21 23:32:04 +0000

    add option into console

--- isomorphic/platforms/console/index.mjs
+++ isomorphic/platforms/console/index.mjs
@@ -1,5 +1,28 @@
+import chalk from "chalk";
+import chalkTable from "chalk-table";
+
 export default class TableConsoleComponent {
   render(data) {
-    console.log("asdasdasda console");
+    const columns = this.preparedData(data);
+
+    const options = {
+      leftPad: 2,
+      columns,
+    };
+
+    const table = chalkTable(options, data);
+    console.log(table);
+  }
+
+  preparedData(data) {
+    const [first] = data;
+    const headers = Object.keys(first);
+    const formatHeader = (data, index) =>
+      index % 2 === 0 ? chalk.yellow(data) : chalk.green(data);
+
+    return headers.map((header, i) => ({
+      field: header,
+      name: formatHeader(header, i),
+    }));
   }
 }
