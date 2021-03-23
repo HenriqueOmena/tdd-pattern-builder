f14708efc5d67821a3c2d2c400b25d03339cac3b
HenriqueOmena <88lmao@gmail.com>
2021-03-23 22:21:00 +0000

    some tests with map

--- MapAndSetter/map.js
+++ MapAndSetter/map.js
@@ -1,6 +1,6 @@
 export default function mapAPI() {
   const itemObj = { name: "omena" };
-  const itemMap = new Map([["name", "omena"]]);
+  const itemMap = new Map([["name", "henrique omena"]]);
 
   console.log(itemObj);
   console.log(itemMap);
@@ -11,6 +11,32 @@ export default function mapAPI() {
   // check if has a prop
   itemObj.hasOwnProperty("birthDay"); //?
   itemMap.has("birthDay"); //?
+
+  delete itemObj.birthDay; //?
+  itemMap.delete("birthDay"); //?
+
+  Object.keys(itemObj).length; //?
+  itemMap.size; //?
+
+  //get all values
+  Object.entries(itemObj); // ?
+  [...itemMap]; //?
+
+  for (const [key, value] of Object.entries(itemObj)) {
+    key;
+    value;
+  }
+
+  for (const [key, value] of itemMap) {
+    key;
+    value;
+  }
+
+  [...itemMap.keys()]; //?
+
+  // clean all object
+  Object.keys(itemObj).map((key) => delete itemObj[key]); //?
+  itemMap.clear();
 }
 
 mapAPI();
